7c1830e9bc166c331feb2d741b1ab639ecc7a8d9
seegan <see.gan21@gmail.com>
2017-09-21 19:03:35 +0530

    hiring bill

--- wp-content/themes/shc/admin/billing/hiring/class-hiring.php
+++ wp-content/themes/shc/admin/billing/hiring/class-hiring.php
@@ -307,7 +307,7 @@ ON bill.id = sdd.id WHERE bill.bill_qty > 0 AND
 
 			/*$detail_query = "SELECT hd.*, l.product_name, l.product_type FROM ${hiring_detail_table} as hd JOIN ${lot_table} as l ON hd.lot_id = l.id WHERE hd.hiring_bill_id = ${bill_id} AND hd.active = 1";*/
 
-			$detail_query = "SELECT hd.lot_id, SUM(hd.qty) as qty, hd.bill_from, hd.bill_to, hd.bill_days, hd.rate_per_day, SUM(hd.amount) as amount, hd.min_checked, SUM(hd.hiring_amt),   l.product_name, l.product_type FROM ${hiring_detail_table} as hd JOIN ${lot_table} as l ON hd.lot_id = l.id WHERE hd.hiring_bill_id = ${bill_id} AND hd.active = 1 GROUP BY hd.lot_id, hd.bill_days, hd.rate_per_day, hd.min_checked, hd.bill_from, hd.bill_to";
+			$detail_query = "SELECT hd.lot_id, SUM(hd.qty) as qty, hd.bill_from, hd.bill_to, hd.bill_days, hd.rate_per_day, SUM(hd.amount) as amount, hd.min_checked, SUM(hd.hiring_amt) as hiring_amt,   l.product_name, l.product_type FROM ${hiring_detail_table} as hd JOIN ${lot_table} as l ON hd.lot_id = l.id WHERE hd.hiring_bill_id = ${bill_id} AND hd.active = 1 GROUP BY hd.lot_id, hd.bill_days, hd.rate_per_day, hd.min_checked, hd.bill_from, hd.bill_to";
 
 			$data['hiring_detail'] = $wpdb->get_results($detail_query);
 
@@ -332,7 +332,7 @@ ON bill.id = sdd.id WHERE bill.bill_qty > 0 AND
 
 			/*$detail_query = "SELECT hd.*, l.product_name, l.product_type FROM ${hiring_detail_table} as hd JOIN ${lot_table} as l ON hd.lot_id = l.id WHERE hd.hiring_bill_id = ${bill_id} AND hd.active = 1";*/
 
-			$detail_query = "SELECT hd.lot_id, SUM(hd.qty) as qty, hd.bill_from, hd.bill_to, hd.bill_days, hd.rate_per_day, SUM(hd.amount) as amount, hd.min_checked, SUM(hd.hiring_amt),   l.product_name, l.product_type FROM ${hiring_detail_table} as hd JOIN ${lot_table} as l ON hd.lot_id = l.id WHERE hd.hiring_bill_id = ${bill_id} AND hd.active = 1 GROUP BY hd.lot_id, hd.bill_days, hd.rate_per_day, hd.min_checked, hd.bill_from, hd.bill_to";
+			$detail_query = "SELECT hd.lot_id, SUM(hd.qty) as qty, hd.bill_from, hd.bill_to, hd.bill_days, hd.rate_per_day, SUM(hd.amount) as amount, hd.min_checked, SUM(hd.hiring_amt) as hiring_amt,   l.product_name, l.product_type FROM ${hiring_detail_table} as hd JOIN ${lot_table} as l ON hd.lot_id = l.id WHERE hd.hiring_bill_id = ${bill_id} AND hd.active = 1 GROUP BY hd.lot_id, hd.bill_days, hd.rate_per_day, hd.min_checked, hd.bill_from, hd.bill_to";
 
 			$data['hiring_detail'] = $wpdb->get_results($detail_query);
 			return $data;
--- wp-content/themes/shc/template-hiring-invoice.php
+++ wp-content/themes/shc/template-hiring-invoice.php
@@ -1,20 +1,24 @@
 <?php
 /**
- * Template Name: Hiring Invoice 
+ * Template Name: Deposit Invoice 
  *
  * @package WordPress
  * @subpackage SHC
  */
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/admin/inc/css/ultra-colors.css';?>" type="text/css" media="all" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/admin/inc/css/ultra-admin.css';?>" type="text/css" media="all" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/admin/inc/css/bootstrap.min.css';?>" type="text/css" media="all" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/admin/inc/css/custom.min.css';?>" type="text/css" media="all" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/admin/inc/images/invoice/fonts/stylesheet.css';?>" type="text/css" media="all" />
 </head>
 <body class="page">
+
+
 <?php
 $hiring_data = false;
 $bill_data = getHiringBillDataPrint($_GET['bill_no']);
@@ -36,13 +40,42 @@ if(isset($bill_data['hiring_data']) && isset($_GET['bill_no']) && $_GET['bill_no
 	$bill_number = billNumberText($company_id, $bill_data['hiring_data']->bill_no, 'HB');
 
 	$tax_for = $hiring_data->tax_from;
+
+	$gst_total = $hiring_data->cgst_amt + $hiring_data->sgst_amt + $hiring_data->igst_amt;
 }
 
-/*echo "<pre>";
-var_dump($hiring_data);
-echo "</pre>";
-*/
+	$pages = false;
+	$per_page = 16;
+	$pieces = false;
+	$tota_row = 0;
+
+	if($hiring_data) {
+		$pages = ceil(count($bill_data['hiring_detail'])/$per_page);
+		$pieces = array_chunk($bill_data['hiring_detail'], $per_page);
+		$tota_row = count($bill_data['hiring_detail']);
+		$reminder = ($tota_row % $per_page);
+/*				$page_short = false;
+		if($reminder > 12) {
+			$pages = $pages + 1;
+			$page_short = true;
+		}*/
+	}
+
+
+
+	$page_total[-1] = 0;
+	for ($i = 0; $i < $pages; $i++) { 
+		$tot_tmp = 0;
+		foreach ($pieces[$i] as $key => $h_value) {
+			$tot_tmp = $tot_tmp + $h_value->hiring_amt;
+		}
+		$page_total[$i] = $page_total[$i-1] + $tot_tmp;
+	}
+
+
 ?>
+
+
 	<style type="text/css">
 
 		@page {
@@ -146,8 +179,23 @@ echo "</pre>";
 				font-size: 14px;
     			text-decoration: underline;
 			}
+			h3 {
+				margin-top: 0px;
+			}
+/*.footer {
+  background: #ade6df;
+  color: #fff;
+  text-align: center;
+  font-family: 'Open Sans', sans-serif;
+  padding: 5px 0;
+  font-weight: 300;
+  position: fixed;
+  bottom: 0;
+  width: 715px;
+}*/
 
 		}
+
 
 
 			.body {
@@ -226,7 +274,7 @@ echo "</pre>";
 			}
 
 			.table>tbody>tr>td {
-				padding: 0 3px;px;
+				padding: 0 3px;
 				height: 20px;
 			}
 			.table-bordered>tbody>tr>td, .table-bordered>thead>tr>th {
@@ -240,12 +288,14 @@ echo "</pre>";
 				font-size: 14px;
     			text-decoration: underline;
 			}
+			h3 {
+				margin-top: 0px;
+			}
 	</style>
 
 
 
-
-<table class="" > 
+<table class=""> 
 	<thead>
 		<tr>
 			<td>
@@ -292,57 +342,23 @@ echo "</pre>";
 		</tr>
 	</thead>
 	<tbody>
-		<tr>
-			<td>
 
-
-
-
-
-
-
-
-<table class=""> 
-
-	<tbody>
 
 
 		<?php
-			$pages = false;
-			$per_page = 16;
-			$pieces = false;
-			$tota_row = 0;
-
-			if($hiring_data) {
-				$pages = ceil(count($bill_data['hiring_detail'])/$per_page);
-				$pieces = array_chunk($bill_data['hiring_detail'], $per_page);
-				$tota_row = count($bill_data['hiring_detail']);
-				$reminder = ($tota_row % $per_page);
-/*				$page_short = false;
-				if($reminder > 12) {
-					$pages = $pages + 1;
-					$page_short = true;
-				}*/
-			}
-
-
 			for ($i = 0; $i < $pages; $i++) { 
 				$page_start = ( $i * $per_page ) + 1;
-
 				$current_page = ($i + 1);
 		?>
 			<tr>
 				<td>
-					<div class="inner-container" style="margin-top: 1px;">
+					<div class="inner-container" style="margin-top: 0px;">
 						<div class="bill-detail">
-
-
-
-							<table class="table table-bordered">
+							<table class="table table-bordered" style="margin-bottom: 2px;">
 								<thead>
 									<tr>
 										<th colspan="4">
-											<div style="min-height: 100px;padding:10px;">
+											<div style="min-height: 100px;padding:5px;">
 												<div style="line-height:10px;">
 													To: 
 												</div>
@@ -359,7 +375,7 @@ echo "</pre>";
 											</div>
 										</th>
 										<th colspan="4">
-											<div style="min-height: 100px;padding:10px;">
+											<div style="min-height: 100px;padding:5px;">
 												<div>
 													<div style="line-height: 20px;height: 25px;">
 														<div style="float:left;width: 60px">BILL NO</div>
@@ -418,22 +434,26 @@ echo "</pre>";
 
 
 								<?php
-									if($current_page > 1) {
+								if($current_page > 1) {
 								?>
-										<tr>
-											<td></td>
-											<td>
-												<div class="text-center">BF / TOTAL</div>
-											</td>
-											<td><div class="text-center">-</div></td>
-											<td><div class="text-center">-</div></td>
-											<td><div class="text-center">-</div></td>
-											<td><div class="text-center">-</div></td>
-											<td><div class="text-right">-</div></td>
-											<td></td>
-										</tr>
+									<tr>
+										<td></td>
+										<td>
+											<div class="text-center">BF / TOTAL</div>
+										</td>
+										<td><div class="text-center">-</div></td>
+										<td><div class="text-center">-</div></td>
+										<td><div class="text-center">-</div></td>
+										<td><div class="text-center">-</div></td>
+										<td><div class="text-right">-</div></td>
+										<td>
+											<div class="text-right">
+												<?php echo moneyFormatIndia( number_format($page_total[$i-1],2) ); ?>
+											</div>
+										</td>
+									</tr>
 								<?php
-									}
+								}
 								foreach ($pieces[$i] as $key => $value) {
 								?>
 									<tr>
@@ -468,12 +488,12 @@ echo "</pre>";
 										</td>
 										<td>
 											<div class="text-rigth">
-												<?php echo $value->rate_per_day; ?>
+												<?php echo moneyFormatIndia($value->rate_per_day); ?>
 											</div>
 										</td>
 										<td>
 											<div class="text-rigth">
-												<?php echo $value->amount; ?>
+												<?php echo moneyFormatIndia($value->hiring_amt); ?>
 											</div>
 										</td>
 									</tr>
@@ -482,8 +502,6 @@ echo "</pre>";
 								<?php
 									$page_start++;
 								}
-
-
 									if($pages == $current_page) {
 								?>
 										<tr>
@@ -500,7 +518,7 @@ echo "</pre>";
 											<td colspan="7">MRR Transport & Unloading </td>
 											<td>
 												<div class="text-rigth">
-													<?php echo $hiring_data->transportation_charge; ?>
+													<?php echo moneyFormatIndia($hiring_data->transportation_charge); ?>
 												</div>
 											</td>
 										</tr>
@@ -511,49 +529,92 @@ echo "</pre>";
 											<td colspan="7"><div class="text-center">Discount</div></td>
 											<td>
 												<div class="text-right">
-													<?php echo $hiring_data->discount_amount; ?>
+													<?php echo moneyFormatIndia($hiring_data->discount_amount); ?>
 												</div>
 											</td>
 										</tr>
 										<?php
 											}
 										?>
-
 										<tr>
 											<td colspan="7"><div class="text-center">Taxable Amount</div></td>
 											<td>
-												<div class="text-right"><?php echo $hiring_data->total_after_discount; ?></div>
-											</td>
-										</tr>
-										<tr>
-											<td colspan="7"><div class="text-center"><b>CGST - 9%</b></div></td>
-											<td>
-												<div class="text-rigth">
-													<?php echo $hiring_data->cgst_amt; ?>
-												</div>
-											</td>
-										</tr>
-										<tr>
-											<td colspan="7"><div class="text-center"><b>SGST - 9%</b></div></td>
-											<td>
-												<div class="text-rigth">
-													<?php echo $hiring_data->sgst_amt; ?>
-												</div>
-											</td>
-										</tr>
-										<tr>
-											<td colspan="7"><div class="text-center">Total Including Tax</div></td>
-											<td>
 												<div class="text-right">
-													<?php echo $hiring_data->tax_include_tot; ?>
+													<?php echo moneyFormatIndia($hiring_data->total_after_discount); ?>
 												</div>
 											</td>
 										</tr>
+										<?php
+
+										if($hiring_data->tax_from != 'tax_from') {
+
+											if($hiring_data->tax_from == 'gst') {
+
+												if($hiring_data->gst_for == 'cgst') {
+
+										?>
+												<tr>
+													<td colspan="7"><div class="text-center"><b>CGST - 9%</b></div></td>
+													<td>
+														<div class="text-rigth">
+															<?php echo moneyFormatIndia($hiring_data->cgst_amt); ?>
+														</div>
+													</td>
+												</tr>
+												<tr>
+													<td colspan="7"><div class="text-center"><b>SGST - 9%</b></div></td>
+													<td>
+														<div class="text-rigth">
+															<?php echo moneyFormatIndia($hiring_data->sgst_amt); ?>
+														</div>
+													</td>
+												</tr>
+										<?php
+												}
+												if($hiring_data->gst_for == 'igst') {
+										?>
+												<tr>
+													<td colspan="7"><div class="text-center"><b>IGST - 18%</b></div></td>
+													<td>
+														<div class="text-rigth">
+															<?php echo moneyFormatIndia($hiring_data->igst_amt); ?>
+														</div>
+													</td>
+												</tr>
+										<?php
+												}
+											}
+											if($hiring_data->tax_from == 'vat') {
+											?>
+												<tr>
+													<td colspan="7"><div class="text-center"><b>VAT - 5%</b></div></td>
+													<td>
+														<div class="text-rigth">
+															<?php echo moneyFormatIndia($hiring_data->vat_amt); ?>
+														</div>
+													</td>
+												</tr>
+											<?php
+											}
+											?>
+
+											<tr>
+												<td colspan="7"><div class="text-center">Total Including Tax</div></td>
+												<td>
+													<div class="text-right">
+														<?php echo moneyFormatIndia($hiring_data->tax_include_tot); ?>
+													</div>
+												</td>
+											</tr>
+										<?php
+										}
+										?>
+
 										<tr>
 											<td colspan="7"><div class="text-center">Round off</div></td>
 											<td>
 												<div class="text-right">
-													<?php echo $hiring_data->round_off; ?>
+													<?php echo moneyFormatIndia($hiring_data->round_off); ?>
 												</div>
 											</td>
 										</tr>
@@ -561,13 +622,24 @@ echo "</pre>";
 											<td colspan="7"><div class="text-center"><b>Total</b></div></td>
 											<td>
 												<div class="text-right">
-													<?php echo $hiring_data->hiring_total; ?>
+													<?php echo moneyFormatIndia($hiring_data->hiring_total); ?>
 												</div>
 											</td>
 										</tr>
 
+										<table>
+											<tr>
+												<td>Amount Chargable (in words)</td>
+											</tr>
+											<tr>
+												<td><b>INR <?php echo ucwords(convert_number_to_words_full($hiring_data->hiring_total)); ?></b></td>
+											</tr>
+										</table>
 
-										<table class="table table-bordered">
+							<?php
+								if($hiring_data->tax_from != 'tax_from') {
+							?>
+										<table class="table table-bordered" style="margin-top:10px;margin-bottom: 5px;">
 											<thead>
 												<tr>
 													<th class="center-th" style="" rowspan="2">
@@ -625,7 +697,7 @@ echo "</pre>";
 													</td>
 													<td>
 														<div class="text-right">
-															<?php echo $hiring_data->total_after_discount; ?>
+															<?php echo moneyFormatIndia($hiring_data->total_after_discount); ?>
 														</div>
 													</td>
 													<?php 
@@ -638,7 +710,7 @@ echo "</pre>";
 															</td>
 															<td>
 																<div class="text-right">
-																	<?php echo $hiring_data->cgst_amt; ?>
+																	<?php echo moneyFormatIndia($hiring_data->cgst_amt); ?>
 																</div>
 															</td>
 															<td>
@@ -648,7 +720,7 @@ echo "</pre>";
 															</td>
 															<td>
 																<div class="text-right">
-																	<?php echo $hiring_data->sgst_amt; ?>
+																	<?php echo moneyFormatIndia($hiring_data->sgst_amt); ?>
 																</div>
 															</td>
 													<?php
@@ -662,7 +734,7 @@ echo "</pre>";
 															</td>
 															<td>
 																<div class="text-right">
-																	<?php echo $hiring_data->igst_amt; ?>
+																	<?php echo moneyFormatIndia($hiring_data->igst_amt); ?>
 																</div>
 															</td>
 													<?php
@@ -677,7 +749,7 @@ echo "</pre>";
 													</td>
 													<td>
 														<div class="text-right">
-															<?php echo $hiring_data->total_after_discount; ?>
+															<?php echo moneyFormatIndia($hiring_data->total_after_discount); ?>
 														</div>
 													</td>
 													<td>
@@ -690,23 +762,22 @@ echo "</pre>";
 													?>
 														<td>
 															<div class="text-right">
-																<?php echo $hiring_data->cgst_amt; ?>
+																<?php echo moneyFormatIndia($hiring_data->cgst_amt); ?>
 															</div>
 														</td>
 														<td></td>
 														<td>
 															<div class="text-right">
-																<?php echo $hiring_data->sgst_amt; ?>
+																<?php echo moneyFormatIndia($hiring_data->sgst_amt); ?>
 															</div>														
 														</td>
 													<?php 
 														}
 														if($hiring_data->gst_for == 'igst') {
 													?>
-														<td></td>
 														<td>
 															<div class="text-right">
-																<?php echo $hiring_data->igst_amt; ?>
+																<?php echo moneyFormatIndia($hiring_data->igst_amt); ?>
 															</div>	
 														</td>
 													<?php
@@ -714,22 +785,33 @@ echo "</pre>";
 													?>
 												</tr>
 											</tbody>
-
+										</table>
+										<table>
+											<tr>
+												<td>Tax Amount (in words)</td>
+											</tr>
+											<tr>
+												<td><b>INR <?php echo ucwords(convert_number_to_words_full($gst_total)); ?></b></td>
+											</tr>
 										</table>
 								<?php
+								}
 									} else {
 								?>
 										<tr>
 											<td colspan="7">
 												<div class="text-center">CF / TOTAL</div>
 											</td>
-											<td></td>
+											<td>
+												<div class="text-right">
+													<?php echo moneyFormatIndia( number_format($page_total[$i],2) ); ?>
+												</div>
+											</td>
 										</tr>
 								<?php
 									}
 
 								?>
-
 								
 							</table>
 						</div>
@@ -742,87 +824,39 @@ echo "</pre>";
 	</tbody>
 </table>
 
+<div class="footer" style="margin-bottom:20px;">
 
-
-
-
-
-
-
-
-			</td>
-		</tr>
-	</tbody>
-</table>
-
-
-
-<div class="footer" style="margin-bottom:25px;">
-		<div class="inner-container">
-			<div  class="left-float" style="width: 434px">
-				<div style="width: 100%;">
-					<div class="left-float">Rupees</div>
-					<div class="left-float" style="min-width: 360px;border-bottom: 1px dotted;height: 20px;margin-left: 5px;">
-						<?php echo ucfirst(convert_number_to_words_full($invoice_data->total_ninety_days)); ?>
-					</div>
-					<div class="clear"></div>
-				</div>
-				<div class="">
-					<div style="width: 310px;float: left;padding-right: 10px;font-size: 10px;margin-top: 20px;">The Security Deposit will be returned on immediately on safe receipt of the hired equipments * Rental peroid - minimum 30 days * No part payment of the Security Deposit will be returned on receipt of the part quantity of the hired equipments. </div>
-					<div style="float: left;width: 124px;padding: 0 10px;text-align: right;">
-						<div style="margin-top: 5px; ">
-							Received by <br>Cash / Cheque
-						</div>
-
-					</div>
-					<div class="clear"></div>
-				</div>
-			</div>
-			<div class="left-float" style="width:200px;">
-				<table class="table table-bordered" style="margin-bottom: 0px;">
-					<tr>
-						<td style="width: 110px;">Cheque No <span style="float: right;">:</span> </td>
-						<td><?php  echo ($invoice_data->cheque_no !== '0.00') ? $invoice_data->cheque_no : ''; ?></td>
-					</tr>
-					<tr>
-						<td>Date <span style="float: right;">:</span> </td>
-						<td><?php echo ($invoice_data->cheque_date !== '0000-00-00') ? $invoice_data->cheque_date : ''; ?></td>
-					</tr>
-					<tr>
-						<td>amount Rs <span style="float: right;">:</span> </td>
-						<td><?php echo ($invoice_data->cheque_amount !== '0') ? $invoice_data->cheque_amount : ''; ?></td>
-					</tr>
-				</table>
-			</div>
-			<div class="clear"></div>
-		</div>
 
 
 
 
 		<div class="inner-container" style="margin-top: 5px;">
 
-
-			<div class="left-float" style="width: 310px;float: left;padding-right: 10px;">
-				<div>Hirer Signature</div>
-				<div style="border-bottom: 1px dotted;height: 20px;margin-left: 5px;"></div>
-				<div style="width: 310px;float: left;padding-right: 10px;font-size: 10px;margin-top: 2px;">Received the above construction equipments in good condition</div>
+			<div class="left-float" style="width: 480px;float: left;padding-right: 10px;">
+				<div style="float: left;padding-right: 10px;font-size: 10px;margin-top: 30px;">
+					Immediate interest @ 24% p.a. will be charged if not paid within 3 days from the date of the bill
+					<ul style="margin-bottom: 2px;">
+						<li>All materials should be returned thoroughly cleaned and oiled</li>
+						<li>White waste oil should be used for oiling all materials</li>
+						<li>For all materials minimum hire charges will be for 30 days</li>
+					</ul>
+				</div>
+				<div class="clear"></div>
 			</div>
-			<div class="left-float" style="width: 170px;float: left;padding: 0 10px;">
-				WORKING HOURS<br>
-				9.00 AM to 5.00 PM<br>
-				SUNDAY HOLIDAY
-			</div>
-			<div class="left-float" style="width: 154px;">
-				<div style="margin-top: -10px;">For JBC Associates</div>
+			<div class="left-float" style="width: 154px;margin-top:15px;">
+				<div class="company-name" style="font-family: serif;font-weight: bold;font-size: 16px;">
+					For <?php echo $company_data->company_name; ?>
+				</div>
 				<div style="margin-top: 30px;">Manager / Accountant</div>
 			</div>
 			<div class="clear"></div>
 		</div>
 
-		<div class="inner-container foot" style="width: 794px;line-height: 25px;font-size: 14px;color: #fff !important;">
-			<div class="left-float" style="width:317px;font-size: 14px;color: #fff !important;text-align: center;">Email : infojbcaccesss@gmail.com</div>
-			<div class="left-float" style="width:317px;font-size: 14px;color: #fff !important;text-align: center;">Website : www.jcbascdfdsgdfg.in</div>
+
+
+		<div class="inner-container foot" style="width: 810px;line-height: 20px;font-size: 14px;color: #fff !important;">
+			<div class="left-float" style="width:325px;font-size: 14px;color: #fff !important;text-align: center;">Email : infojbcaccesss@gmail.com</div>
+			<div class="left-float" style="width:325px;font-size: 14px;color: #fff !important;text-align: center;">Website : www.jcbascdfdsgdfg.in</div>
 			<div class="clear"></div>
 		</div>
 </div>
